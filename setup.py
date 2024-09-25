fc00d3bc7d6d60ba8796070f7a6e88ca0035a936
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-09-25 16:45:18 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Image-Toolbox',
+    version="0.1.0",
+    description="A short description of your package",
+    packages=find_packages(),  # Automatically find and include your package
+    install_requires=[         # List of dependencies
+      "SimpleITK",
+      "ipywidgets",
+      "matplotlib"
+    ],
+    classifiers=[              # Metadata
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.8',
+)
