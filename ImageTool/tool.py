7c1da81b8b71fb9647cbbb11d2e0bfcb877e4c9e
Qiyu Zhang <148504334+Qiyu-Zh@users.noreply.github.com>
2024-09-25 16:47:01 -0700

    Create tool.py

---
+++ ImageTool/tool.py
@@ -0,0 +1,7 @@
+import SimpleITK as sitk
+import ipywidgets as widgets
+from IPython.display import display
+import numpy as np
+from ipywidgets import interact
+from ipywidgets.widgets import IntSlider
+import matplotlib.pyplot as plt
